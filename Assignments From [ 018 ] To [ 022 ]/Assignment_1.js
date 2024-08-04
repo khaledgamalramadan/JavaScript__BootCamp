9c5e7655b5b35aeecc96b04d5a0bbe5eedfd345b
khaledgamalramadan <bebo50016@gmail.com>
2024-08-04 17:46:02 +0300

    add a new assignments in js

---
+++ Assignments From [ 018 ] To [ 022 ]/Assignment_1.js
@@ -0,0 +1,8 @@
+
+// Replace ? With Arithmetic Operators
+// cass one
+console.log((10 * 20) + 190 + 10 - 15 % 3 - 400); // 0
+
+// cass two
+console.log(10 * 20 - (15 % 3) + 190 + 10 - 400); // 0
+
